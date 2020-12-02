6d0a1fbf6693b75fc7b10438a80772252c5e6d37
crs2956 <charlessidles2021@u.northwestern.edu>
2020-12-01 19:19:43 -0600

    Initial Commit

---
+++ project.js
@@ -0,0 +1,7 @@
+
+
+function learnMore() {
+    document.getElementById("vid_description").style.opacity = 1;
+    document.getElementById("pod_description").style.opacity = 1;
+    document.getElementById("learn_more").style.display = "none";
+}
